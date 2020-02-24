0fe0354bb93f1a2a1f7d9e494feca74adc86c1d3
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-24 13:50:30 +0000

    final code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
+  get '/users/new', to: 'users#new', as: 'register' #updating the path users go to in order to register for a site.
+  #Now the application lets programmers use register_path when creating links with link_to
 end
