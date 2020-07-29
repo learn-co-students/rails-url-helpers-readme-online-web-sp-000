2508d9121f711ead727b7c25000e270890c283ef
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-29 11:47:48 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
 end
+
+#This will create routing methods for posts that we can utilize in our views and controllers. Running rails routes in the terminal will give the following output:
+
+#If we had a User model/controller:
+#get '/users/new', to: 'users#new', as: 'register'
+#can now use register_path when creating links with link_to 
